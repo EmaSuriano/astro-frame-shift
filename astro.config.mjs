f591b6edc389492e67dbbe6cdd5a29c43cb70df3
Emanuel Suriano <emanuel.suriano@revolut.com>
2025-08-12 15:46:22 +0200

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import compress from '@playform/compress';
+
+const isDev = import.meta.env.DEV;
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://emasuriano.github.io',
-  base: 'astro-frame-shift',
+  base: isDev ? '' : 'astro-frame-shift',
   prefetch: { defaultStrategy: 'viewport' },
-  integrations: [compress()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
   // Authorizing remote images https://images.unsplash.com/
-  image: {
-    remotePatterns: [{ protocol: 'https' }],
-  },
+  image: { remotePatterns: [{ protocol: 'https' }] },
 });
