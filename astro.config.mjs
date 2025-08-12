fb545a8118cfb893f9ee8ccae9a74471d256be68
Emanuel Suriano <emanuel.suriano@revolut.com>
2025-08-12 14:41:04 +0200

    add astro compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import compress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://emasuriano.github.io",
-  base: "astro-frame-shift",
+  site: 'https://emasuriano.github.io',
+  base: 'astro-frame-shift',
   prefetch: {
-    defaultStrategy: "viewport",
+    defaultStrategy: 'viewport',
   },
+  integrations: [compress()],
   vite: {
     plugins: [tailwindcss()],
+  },
+  image: {
+    remotePatterns: [{ protocol: 'https' }],
   },
 });
